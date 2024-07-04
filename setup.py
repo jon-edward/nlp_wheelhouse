177a402e0ce76000f3661ab8b85ea12b10bdfc2b
jon-edward <townsend.jonathan.e@gmail.com>
2024-07-04 14:27:03 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from setuptools import setup
+
+
+with Path(__file__).parent.joinpath("requirements.txt").open() as f:
+    requirements = f.readlines()
+
+setup(
+    name="nlp_wheelhouse",
+    url="https://github.com/jon-edward/nlp_wheelhouse",
+    license="License :: OSI Approved :: MIT License",
+    install_requires=requirements,
+    python_requires=">=3.10.0",
+    description="This is a wrapper library for packaging various NLP tools.",
+)
